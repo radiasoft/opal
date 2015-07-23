Fixing the sign flip bug required introducing k_i = 0 vector components, which were absent from the previous iteration. This introduced nans in the shape function from divide-by-zero errors which are just 1. for that shape function. Introduced logic to fix this. This fixes the sign flip across the boundary and the shape function bugs.

examples/two-body_problem.py
@@ -52,9 +52,9 @@ class periodic_boundary:
 my_boundary = periodic_boundary(simulation_lengths)
 
 # Field properties
-n_modes = 50
+n_modes = 15
 delta_k = 2*np.pi/simulation_lengths
-macro_size = 0.05
+macro_size = 0.015
 
 
 # The params_dictionary for the electrostatic field + particles

opal/fields/discrete_fourier_electrostatic.py
@@ -2,7 +2,7 @@ __author__ = 'swebb'
 
 
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 class discrete_fourier_electrostatic:
 
@@ -15,15 +15,17 @@ class discrete_fourier_electrostatic:
 
         # Use linear strides to generate the k-space vectors
         # Positive and negative k-vectors
+        kmin = np.zeros(dims)
+        for idx in range(0, dims):
+            kmin[idx] = -self.dk[idx]*self.n_modes[idx]
+
         for idx in range(dims):
             self.n_modes[idx] *= 2
+            self.n_modes[idx] += 1
         num_modes = np.prod(self.n_modes)
 
         self.k_vectors = np.zeros((num_modes, dims))
-        self.k_squared = np.zeros(num_modes)
-        kmin = np.zeros(dims)
-        for idx in range(0, dims):
-            kmin[idx] = -0.5*self.dk[idx]*self.n_modes[idx] + 0.5*self.dk[idx]
+        self.k_squared = np.ones(num_modes)
         for idx in range(0, num_modes):
             linear_stride = idx
             for dim_idx in range(0, dims):
@@ -32,8 +34,9 @@ class discrete_fourier_electrostatic:
                 linear_stride -= this_idx
                 linear_stride /= self.n_modes[dim_idx]
             self.k_vectors[idx] += kmin
-            self.k_squared[idx] = np.dot(self.k_vectors[idx],
-                                         self.k_vectors[idx])
+            if not all(v == 0. for v in self.k_vectors[idx]):
+                self.k_squared[idx] = np.dot(self.k_vectors[idx],
+                                             self.k_vectors[idx])
 
 
     def compute_fields(self, rho):

opal/interpolaters_depositers/tent_dfes.py
@@ -30,11 +30,16 @@ class tent_dfes:
 
         # This shape function is specific for a tent particle with finite
         # transverse size.
-        self.shape_function = 1.
+        self.shape_function = np.ones(np.shape(self.k_modes)[0])
         for dim in range (0, np.shape(self.k_modes)[1]):
-            self.shape_function *= \
-            (2. - 2.*np.cos(self.ptcl_size[dim]*self.k_modes[:,dim]))/ \
-            (self.ptcl_size[dim]*self.k_modes[:,dim])**2
+            for idx in range(0, np.shape(self.k_modes)[0]):
+                form_factor = \
+                    (2.-2.*np.cos(self.ptcl_size[dim]*
+                                  self.k_modes[idx,dim]))/ \
+                    (self.ptcl_size[dim]*self.k_modes[idx,dim])**2
+                if np.isnan(form_factor):
+                    form_factor = 1.
+                self.shape_function[idx] *= form_factor
 
 
     def compute_forces(self, pos, vel, weights):
@@ -68,12 +73,10 @@ class tent_dfes:
 
         efield *= 1.j
 
-        print 'efield =', efield
-
         bfield = np.zeros(np.shape(efield))
 
         # Truncate imaginary part that is lingering after sum due to roundoff
-        acceleration = self.charge2mass*efield.real
+        acceleration = self.charge2mass*weights*(efield.real)
 
         return acceleration
 