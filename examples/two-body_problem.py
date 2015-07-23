Fixed a bug where the species couldn't handle weights with opposite signs (i.e. electron versus positron stored as the same species, but with opposite weights) consistently. It computed the potential correctly, but then the acceleration wasn't satisfying Newton's Second Law because of missed cancellation. Bug is now fixed.

examples/two-body_problem.py
@@ -51,9 +51,9 @@ class periodic_boundary:
 my_boundary = periodic_boundary(simulation_lengths)
 
 # Field properties
-n_modes = 50
+n_modes = 15
 delta_k = 2*np.pi/simulation_lengths
-macro_size = 0.01
+macro_size = 0.05
 
 
 # The params_dictionary for the electrostatic field + particles
@@ -103,13 +103,12 @@ weight = []
 #the_particles.add_particle(pos, vel)
 pos = [1.1, 0.6]
 vel = [0., 0.]
-weight.append(1.)
-the_particles.add_particle(pos, vel)
+weight = 1.
+the_particles.add_particle(pos, vel, weight)
 pos = [0.9, 0.4]
 vel = [0., 0.]
-weight.append(1.)
-the_particles.add_particle(pos, vel)
-weights = np.array(weight)
+weight = -1.
+the_particles.add_particle(pos, vel, weight)
 # Run the simulation
 ptcl_history = []
 KE = []
@@ -131,9 +130,7 @@ for idx in range(0, nsteps):
     the_particles.move()
     the_depinterp.deposit_sources(the_particles.pos,
                                   the_particles.vel,
-                                  weights)
-    acceleration = the_depinterp.compute_forces(the_particles.pos,
-                                                the_particles.vel)
+                                  the_particles.weights)
     the_particles.accelerate(the_depinterp)
     phi = the_fields.get_fields()
     kvecs = the_fields.get_kvectors()
@@ -145,6 +142,17 @@ for idx in range(0, nsteps):
                extent=[0., 2., 0., 1.],
                origin='lower',
                cmap=mpl.cm.bone_r)
+    plt.colorbar()
+    plt.show()
+
+    plt.clf()
+
+    plt.imshow(potential.imag,
+               extent=[0., 2., 0., 1.],
+               origin='lower',
+               cmap=mpl.cm.bone_r)
+
+    plt.colorbar()
     plt.show()
 
     if idx%100 == 0:

opal/interpolaters_depositers/tent_dfes.py
@@ -37,7 +37,7 @@ class tent_dfes:
             (self.ptcl_size[dim]*self.k_modes[:,dim])**2
 
 
-    def compute_forces(self, pos, vel):
+    def compute_forces(self, pos, vel, weights):
         """ Calculates the forces for each particle based on the fields.
         For the discrete Fourier electrostatic class, this is a simple
         electric field acceleration that includes no magnetic rotation.
@@ -64,12 +64,14 @@ class tent_dfes:
             fourier = np.exp(1.j*(np.dot(pos[idx], self.k_modes.T)))
             fourier *= coeffs
             efield[idx] = np.dot(fourier, self.k_modes)
+            efield[idx] *= weights[idx]
 
         efield *= 1.j
 
         bfield = np.zeros(np.shape(efield))
 
-        acceleration = self.charge2mass*efield
+        # Truncate imaginary part that is lingering after sum due to roundoff
+        acceleration = self.charge2mass*efield.real
 
         return acceleration
 

opal/particles/non_rel_ptcl.py
@@ -11,6 +11,7 @@ class non_rel_ptcl:
         self.dimension = pd['dimensions']
         self.pos = np.zeros((self.num_ptcls, self.dimension))
         self.vel = np.zeros((self.num_ptcls, self.dimension))
+        self.weights = np.zeros(self.num_ptcls)
         self.exists = [False]*self.num_ptcls
         self.dt = pd['dt']
 
@@ -32,7 +33,8 @@ class non_rel_ptcl:
 
     def accelerate(self, depinterp):
 
-        acceleration = depinterp.compute_forces(self.pos, self.vel)
+        acceleration = depinterp.compute_forces(self.pos, self.vel,
+                                                self.weights)
         self.vel += self.dt * acceleration
 
 
@@ -41,13 +43,14 @@ class non_rel_ptcl:
         return self.pos, self.vel
 
 
-    def add_particle(self, position, velocity):
+    def add_particle(self, position, velocity, weight):
 
         added_ptcl = False
         for idx in range(0, self.num_ptcls):
             if not self.exists[idx]:
                 self.pos[idx,:] = position[:]
                 self.vel[idx,:] = velocity[:]
+                self.weights[idx] = weight
                 self.exists[idx] = True
                 added_ptcl = True
                 break
@@ -57,6 +60,7 @@ class non_rel_ptcl:
             np.append(np.zeros(self.dimension), self.vel)
             self.pos[-1,:] = position[:]
             self.vel[-1,:] = velocity[:]
+            self.weights[idx] = weight
             self.exists.append(True)
 
             added_ptcl = True