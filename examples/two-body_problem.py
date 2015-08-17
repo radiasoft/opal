There is a bug in the energy computation which seems related to some conspiracy of sign errors in the particle move that leads to poor energy behavior.

examples/two-body_problem.py
@@ -26,7 +26,7 @@ import scipy.signal as signal
 
 dimensions = 2
 dt = 1.e-9
-nsteps = 10**6
+nsteps = 10**5
 plot_potential = False
 plot_diagnostics = True
 

opal/fields/discrete_fourier_electrostatic.py
@@ -13,8 +13,6 @@ class discrete_fourier_electrostatic:
         self.dk = params_dictionary['delta k']
         dims = params_dictionary['dimensions']
 
-        self.fourier_factor = (2.*np.pi)**dims
-
         # Use linear strides to generate the k-space vectors
         # Positive and negative k-vectors
         kmin = np.zeros(dims)
@@ -51,7 +49,7 @@ class discrete_fourier_electrostatic:
             the k-vectors
         """
 
-        self.phi = -4.*np.pi*rho/self.k_squared
+        self.phi = 4.*np.pi*np.conj(rho)/self.k_squared
 
         return self.phi
 

opal/interpolaters_depositers/tent_dfes.py
@@ -12,7 +12,6 @@ class tent_dfes:
         self.ptcl_size = pd['particle size']
         self.charge2mass = pd['charge']/pd['mass']
         self.charge = pd['charge']
-        self.fourier_factor = (2.*np.pi)**pd['dimensions']
         self.rho = 0.
 
 
@@ -74,9 +73,9 @@ class tent_dfes:
             fourier = np.exp(1.j*(np.dot(pos[idx], self.k_modes.T)))
             fourier *= coeffs
             efield[idx] = np.dot(fourier, self.k_modes)
-            efield[idx] *= weights[idx]
+            efield[idx] *= weights[idx]/abs(weights[idx])
 
-        efield *= 1.j
+        efield *= -1.j
 
         bfield = np.zeros(np.shape(efield))
 
@@ -100,7 +99,7 @@ class tent_dfes:
 
         fourier = np.exp(-1.j*np.dot(pos, self.k_modes.T))
         fourier *= self.shape_function
-        self.rho += self.charge*np.dot(weight, fourier)#/self.fourier_factor
+        self.rho += self.charge*np.dot(weight, fourier)
 
     def get_rho(self):
 